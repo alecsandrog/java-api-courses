2d7fccafd02ed217c297617b52e146d2ef42209e
Alecsandro <alecsandro.info@gmail.com>
2021-08-21 17:42:42 -0300

    Resource students

---
+++ src/main/java/com/example/entities/Student.java
@@ -0,0 +1,85 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String cpf;
+	private String email;
+
+	public Student() {
+	}
+
+	public Student(int id, String name, String cpf, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return id == other.id;
+	}
+
+}
---
+++ src/main/java/com/example/services/StudentService.java
@@ -0,0 +1,63 @@
+package com.example.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.entities.Student;
+import com.example.repositories.StudentRepository;
+import com.example.services.exceptions.DatabaseException;
+import com.example.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	private StudentRepository repository;
+
+	public List<Student> findAll() {
+		return repository.findAll();
+	}
+
+	public Student findById(int id) {
+		Optional<Student> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(obj));
+	}
+
+	public Student insert(Student obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(int id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Student update(int id, Student obj) {
+		try {
+			Student entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Student entity, Student obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setCpf(obj.getCpf());
+	}
+
+}
